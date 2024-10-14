bd8d82befdd7866dc8620bf37b9510c9b1075349
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-10-15 00:46:16 +0300

    Add forward force to the player

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
 
-    public float rotationSpeed;
+    private float rotationSpeed = 25;
     // Start is called before the first frame update
     void Start()
     {
