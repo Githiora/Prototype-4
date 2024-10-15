90e4c582db95351e66999986a4fde471236e4f1b
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-10-15 21:59:44 +0300

    Randomly generate spawn position

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,17 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomSpawnPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        Instantiate(enemyPrefab, randomSpawnPos, enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
