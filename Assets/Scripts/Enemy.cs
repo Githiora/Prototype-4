5336efc5f8755826a9cf549318e4cb368c645471
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-10-15 21:25:32 +0300

    Create enemy script to follow player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody enemyRb;
+    private float speed = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - enemyRb.transform.position).normalized * speed);
+    }
+}
